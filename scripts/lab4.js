e324c965a18f7d41c59522f7d9e1bf10aa2beef5
Eric Nguyen <nericguyen@gmail.com>
2025-05-02 18:29:48 -0700

    Checked for edge cases in lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,8 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') return false;
+
     if (add) {
         let result = 0;
 
@@ -25,8 +27,14 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if(discount < 0 || discount > 1) return false;
+    if(prices.constructor !== Array) return false; 
+
     let discounted = [];
     let length = prices.length;
+
+    if(length == 0) return false;
+
     let discountedPrice = 0;
     for(let i = 0; i < length; i++) {
         discountedPrice += prices[i] * (1 - discount);
